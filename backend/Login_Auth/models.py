84dab6a4ab55afa7b17cc35dc212ad24654aa725
PrashannaChand <chandprashanna99@gmail.com>
2025-06-11 00:08:48 +0545

    login backend portion

---
+++ backend/Login_Auth/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+# Create your models here.
+class CustomUserManager(BaseUserManager):
+    """
+    Custom manager for the CustomUser model.
+    This allows for creating users with email as the username.
+    """
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        password=user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+    
+class CustomUser(AbstractUser):  
+    """
+    Custom user model that extends the default Django user model.
+    This can be used to add additional fields or methods in the future.
+    """
+    email = models.EmailField(unique=True, blank=False, null=False)
+    username = models.CharField(max_length=150, unique=True, blank=True, null=True)
+    objects = CustomUserManager()   
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+  
