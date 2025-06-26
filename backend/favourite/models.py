1a3e9a99e55b32df68b98e56f7b3e31ec71b5660
PrashannaChand <chandprashanna99@gmail.com>
2025-06-26 23:29:45 +0545

    favourite updated and tested

--- backend/favourite/models.py
+++ backend/favourite/models.py
@@ -6,4 +6,4 @@ class Favourite(models.Model):
     city = models.CharField(max_length=100)
 
     def __str__(self):
-        return f'{self.city} (user: {self.user.email})'
+        return f'{self.city} (user: {self.user.username})'
--- backend/weatherwave_project/settings.py
+++ backend/weatherwave_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "rest_framework_simplejwt",
     "forecast",
     "Login_Auth",
